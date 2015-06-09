a86e95d6f1d56e4f3bc1a343bb2408a0c78091cb
agaland <galand.antoine@gmail.com>
2015-06-08 19:31:45 -0700

    Creation

---
+++ LetterCount/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+//import java.util.HashSet;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		String content=getContent("test.txt");
+		HashMap<String,Integer> map = new HashMap<String,Integer>();
+		count(content, map);
+	}
+
+private static void count(String content, HashMap<String, Integer> map) {
+	char[] tmp = content.toCharArray();	
+	for (int i=0; i< tmp.length-1;i++){
+			char letter=tmp[i];
+		}
+		
+	}
+
+private static String getContent(String filename) {
+	return null;
+}
+
+}
