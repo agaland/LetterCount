1b443ce4a8cbb5c0a3526c9f347e39f0011ea72c
Pierre Rohel <pierre.rohel@servicenow.com>
2015-06-08 19:59:48 -0700

    added file reading method

--- LetterCount/src/Main.java
+++ LetterCount/src/Main.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 //import java.util.HashSet;
 
@@ -11,16 +15,26 @@ public class Main {
 		System.out.println(map.entrySet());
 	}
 
-private static void count(String content, HashMap<String, Integer> map) {
-	char[] tmp = content.toCharArray();	
-	for (int i=0; i< tmp.length-1;i++){
+	private static void count(String content, HashMap<String, Integer> map) {
+		char[] tmp = content.toCharArray();	
+		for (int i=0; i< tmp.length-1;i++){
 			char letter=tmp[i];
 			map.put(String.valueOf(letter), map.get(String.valueOf(letter)+1));
 		}
 	}
 
-private static String getContent(String filename) {
-	return null;
-}
+	private static String getContent(String filename) {
+		try {
+			return readFile(filename, Charset.defaultCharset());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
+	}
 
 }
